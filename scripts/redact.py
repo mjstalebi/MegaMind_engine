0b32b8dc511050334c3cec250ed6827c06d81127
Seyed Mohammadjavad Seyed Talebi <mjavad@uci.edu>
2020-05-10 19:50:34 -0700

    Add files

--- scripts/redact.py
+++ scripts/redact.py
@@ -7,16 +7,18 @@ def pr(*args , **kwargs):
 
 pr("redacting extention")
 comm = Commu()
-comm.update_ready(False)
+comm.init_pipes()
+#comm.update_ready(False)
 while True:
-	comm.wait_for_new_data()
-	session = comm.read_data()
+	#comm.wait_for_new_data()
+	#session = comm.read_data()
+	session = comm.read_data_blocking()
 	pr(session)
 	item = comm.get_item_of_interest(session)
 	print(item)
 	item = item.replace('javad','bob')
 	item = item.replace('ardalan','alex')
 	comm.write_response(item)
-	comm.update_ready(True)
+	#comm.update_ready(True)
 
 	
